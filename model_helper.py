fixed loss function and added averages

model_helper.py
@@ -116,6 +116,13 @@ def relu_prime(activation):
 		return 0 if activation < 0 else 1
 
 
+def linear(activation):
+	return activation
+
+
+def linear_prime(activation):
+	return activation
+
 
 def sigmoid(activation):
 	if isinstance(activation, list):
@@ -132,7 +139,7 @@ def sigmoid_prime(activation):
 	else:
 		return  (sigmoid(activation) * (1 - sigmoid(activation)))
 	
-	
+
 
 #def softmax(activation):
 #	print(activation)
@@ -142,7 +149,7 @@ def sigmoid_prime(activation):
 def softmax(activation):
     softmax_activation = []
     for vector_1 in activation:
-        softmax_activation.append([ math.exp(clip(activation_i, -255, 255)) / sum([math.exp(activation_all) for activation_all in vector_1]) for activation_i in vector_1])
+        softmax_activation.append([ math.exp(clip(activation_i, -255, 255)) / sum([math.exp(clip(activation_all, -255, 255)) for activation_all in vector_1]) for activation_i in vector_1])
     
     return softmax_activation
 
@@ -183,21 +190,78 @@ def mean_squared_error(prediction, target):
 	squared_error = [ [ (pred-targ)**2 for pred,targ in zip(prediction_i, target_i)] for prediction_i,target_i in zip(prediction,target) ]
 	summed_up_squared_error = sum_up_matrix_by_cols(squared_error)
 	#print(summed_up_squared_error)
-	return summed_up_squared_error #[ elem / len(squared_error) for elem in summed_up_squared_error]
+	summed_up_squared_error = [val / len(prediction) for val in summed_up_squared_error]
+	return sum(summed_up_squared_error) / len(summed_up_squared_error) #[ elem / len(squared_error) for elem in summed_up_squared_error]
 
 
 #print(mean_squared_error([[0.8488765622021412]],[[1.659]]))
 
-def mean_squared_error_prime(prediction, target):
+def mean_squared_error_prime(prediction, target, loss_average=False):
 	assert len(prediction) == len(target) 
 	assert len(prediction[0]) == len(target[0])
 
-	squared_error = [ [ -(targ - pred) for pred,targ in zip(prediction_i, target_i)] for prediction_i,target_i in zip(prediction,target) ]
-	#print(squared_error)
-	summed_up_squared_error = sum_up_matrix_by_cols(squared_error)
-	return summed_up_squared_error 
+	error = [ [ 2* (pred - targ) for pred,targ in zip(prediction_i, target_i)] for prediction_i,target_i in zip(prediction,target) ]
+	#summed_up_error = sum_up_matrix_by_cols(error)
+	summed_up_error = [ [val_1 / len(vec_1) for val_1 in vec_1] for vec_1 in error]
 
-#print(mean_squared_error_prime([[0.061908245406225415], [0.038427675401711436]] , [[1.659], [3.606]]))
+	if loss_average:
+		summed_up_error_columns = sum_up_matrix_by_cols(summed_up_error)
+		summed_up_error_columns = [ val / len(summed_up_error) for val in summed_up_error_columns]
+		summed_up_error_columns = [summed_up_error_columns] * len(summed_up_error)
+	else:
+		summed_up_error_columns = summed_up_error
+	#summed_up_error = error
+	#print(squared_error)
+	"""
+	if batch_average:
+		#print(squared_error)
+		summed_up_squared_error = sum_up_matrix_by_cols(squared_error)
+		#print(summed_up_squared_error)
+		summed_up_squared_error = [val / len(prediction) for val in summed_up_squared_error]
+		#print(summed_up_squared_error)
+		summed_up_squared_error = [summed_up_squared_error] * len(prediction)
+		#print(summed_up_squared_error)
+	else:
+		#print("äh ja not summed up ne")
+		summed_up_squared_error = squared_error
+	"""
+	#summed_up_squared_error = sum_up_matrix_by_cols(squared_error)
+	return summed_up_error_columns 
+
+#print(mean_squared_error_prime([[1, 3, 4], [2, 4, 5], ] , [[3, 1, 1], [4, 3, 1]], True))
+#print(mean_squared_error_prime([[1, 3, 4], [2, 4, 5], ] , [[3, 1, 1], [4, 3, 1]], False))
+
+import numpy as np
+
+
+#print(ret([[1, 3], [2, 4]] , [[3, 1], [4, 3]]))
+
+
+def MSE_loss_grad(predictions, targets):
+	"""
+	Computes mean squared error gradient between targets 
+	and predictions. 
+	Input: predictions (N, k) ndarray (N: no. of samples, k: no. of output nodes)
+			targets (N, k) ndarray        (N: no. of samples, k: no. of output nodes)
+	Returns: (N,k) ndarray
+	Note: The averaging is only done over the output nodes and not over the samples in a batch.
+	Therefore, to get an answer similar to PyTorch, one must divide the result by the batch size.
+	"""
+
+	predictions = np.array(predictions)
+	targets = np.array(targets)
+		
+	return 2*(predictions-targets)/predictions.shape[1]
+
+
+#print(MSE_loss_grad([[1, 3, 4], [2, 4, 5], ] , [[3, 1, 1], [4, 3, 1]]))
+
+#from sklearn.metrics import mean_squared_error as sck
+#print(mean_squared_error([[1, 3], [2, 4], [4, 4] , [1, 2] ] , [[3, 1], [4, 3], [4, 3], [3, 4] ]))
+#print(sck([[1, 3], [2, 4], [4, 4] , [1, 2] ] , [[3, 1], [4, 3], [4, 3], [3, 4] ]))
+#print(lol([[1, 3], [2, 4]] , [[3, 1], [4, 3]]))
+
+#print(mean_squared_error_prime([[1,2,3,4],[1,2,3,4]] , [[0,1,0,0],[0,0,1,0]]))
 
 #def cross_entropy(predictions, targets):
 #	if isinstance(predictions, list):
@@ -205,19 +269,20 @@ def mean_squared_error_prime(prediction, target):
 #
 #    return sum([ -(math.log(p) * t) if p != 0 else -(math.log(1e-100) * t) for p,t in zip(predictions, targets)])
 
+
 def cross_entropy(predictions, targets):
     cross = []
     for vector_1, vector_2 in zip(predictions, targets):
-        cross.append([ -(math.log(p) * t) if p != 0 else -(math.log(1e-100) * t) for p,t in zip(vector_1, vector_2)])
+        cross.append([ -(math.log(p) * t) if p > 0 else -(math.log(1e-100) * t) for p,t in zip(vector_1, vector_2)])
     
     a = sum_up_matrix_by_cols(cross)
     return a
 
-
+#print(cross_entropy([[1, 3], [2, 4]] , [[3, 1], [4, 3]]))
 #def cross_entropy_prime(predictions, targets):
 #    return -sum([ t / p if p != 0 else ( t / math.log(1e-100) ) for p,t in zip(predictions, targets)])
 
-
+"""
 def cross_entropy_prime(predictions, targets):
 	cross = []
 	for vector_1, vector_2 in zip(predictions, targets):
@@ -225,7 +290,63 @@ def cross_entropy_prime(predictions, targets):
 	a = sum_up_matrix_by_cols(cross)
 	return a
 
-#print(cross_entropy([[1,0,0] , [0,1,0]], [[1,0,0] , [0,1,0]]))
+#print(cross_entropy_prime([[1,2,3,4],[1,2,3,4]] , [[0,1,0,0],[0,0,1,0]]))
+#print(cross_entropy_prime(softmax([[1,2,1,1]]) , [[1,0,0,0]]))
+
+
+import numpy as np
+def cross_entropy(logits,reference_answers):
+	# Compute crossentropy from logits[batch,n_classes] and ids of correct answers
+	logits_for_answers = logits[np.arange(len(logits)),reference_answers]
+	print(logits_for_answers)
+	#logits_for_answers = logits
+
+	xentropy = - logits_for_answers + np.log(np.sum(np.exp(logits),axis=-1))
+
+	return xentropy
+"""
+import numpy as np
+
+def cross_entropy_prime(logits,reference_answers):
+	# Compute crossentropy gradient from logits[batch,n_classes] and ids of correct answers
+	logits = np.array(logits)
+	ones_for_answers = np.array(reference_answers)
+	#ones_for_answers[np.arange(len(logits)),reference_answers] = 1
+	#print(ones_for_answers)
+	ll = np.exp(logits) / np.exp(logits).sum(axis=-1,keepdims=True)
+	a = - ones_for_answers + ll
+	#print("a : ", a)
+	hm = []
+	for vec_1 in a:
+		hm.append([float(b) / logits.shape[0] for b in vec_1])
+	#a = [ float(b) / logits.shape[0]  for b in a for b in a]
+	return  hm #[ h / logits.shape[0] for h in a ]
+
+#print(cross_entropy_prime([[1,2,1,1]] , [[1,0,0,0]]))
+#print(cross_entropy_prime([[1, 3], [2, 4]] , [[3, 1], [4, 3]]))
+#print(cross_entropy_prime([[1, 3, 2, 4]] , [[3, 1 , 4, 3]]))
+
+#print(cross_entropy_prime([[1,2,3,4], [1,2,3,4]] , [[0,1,0,0], [0,0,1,0]]))
+#print(cross_entropy_prime([[1,2,3,4],[1,2,3,4]] , [[0,1,0,0],[0,0,1,0]]))
+
+
+
+
+"""
+def cross_entropy_prime(logits,reference_answers):
+    # Compute crossentropy gradient from logits[batch,n_classes] and ids of correct answers
+    ones_for_answers = np.zeros_like(logits)
+    ones_for_answers[np.arange(len(logits)),reference_answers] = 1
+    
+    nn = np.exp(logits) / np.exp(logits).sum(axis=-1,keepdims=True)
+    
+    return (- ones_for_answers + nn) / logits.shape[0]
+"""
+#print(cross_entropy_prime([[1, 3], [2, 4]] , [[3, 1], [4, 3]]))
+#print(cross_entropy_prime([[1, 3, 2, 4]] , [[3, 1 , 4, 3]]))
+
+#print(cross_entropy_prime(np.array([[1,2,3,4], [1,2,3,4]]) , np.array([[1], [2]])))
+#print(cross_entropy_prime(np.array([[1,2,3,4],[1,2,3,4]]) , np.array([1,2])))
 
 def hit_or_miss(predictions, targets, round_function = 6, error_margin = 0.01):
 
@@ -287,6 +408,8 @@ def r_2(predictions, targets):
 function_dictionary = {
 	"relu" 			: relu,
 	"relu_prime" 		: relu_prime,
+	"linear" 			: linear,
+	"linear_prime" 		: linear_prime,
 	"sigmoid"			: sigmoid,
 	"sigmoid_prime"	: sigmoid_prime,
 	"softmax"		: softmax,